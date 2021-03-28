933a9d519fb83ca9f4cd7e2d71e4e5d3af75e4d6
GanescuRobert <studentganescurobertflorinel@gmail.com>
2021-03-28 13:22:32 +0300

    C#

---
+++ LearnC#/DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    //
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        //Get api/values
+        [HttpGet]
+        public async System.Threading.Tasks.Task<IActionResult> GetValuesAsync()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async System.Threading.Tasks.Task<IActionResult> GetValueAsync(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+    }
+}
